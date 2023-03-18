6a81cdc8b3ac16c9ea486904da511e3f30a9ec98
harryjung <harryjung0330@gmail.com>
2023-03-18 14:14:01 +0900

    fixed buildspecy.yaml for code pipeline

--- build/resources/main/data.sql
+++
@@ -1,7 +0,0 @@
-
-MERGE INTO authentication_providers(provider) values('GOOGLE');
-
-MERGE INTO authentication_providers(provider) values('APPLE');
-
-MERGE INTO authentication_providers(provider) values('INTERNAL');
-
