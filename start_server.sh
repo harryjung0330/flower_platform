93011b43af5d835dcd0dea638e9e6cd96a98fbfa
harryjung <harryjung0330@gmail.com>
2023-05-14 22:19:37 +0900

    fixed start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo java -jar -Dlogging.info.path=/home/ec2-user/server.log -Dspring.datasource.url=$(cat /home/ec2-user/dbEndpoint.txt) -Djasypt.encryptor.password=$(cat /home/ec2-user/jasyptKey.txt) -Dspring.profiles.active=test -Dserver.port=80 /home/ec2-user/flower-platform.jar &
+sudo java -jar -Dlogging.info.path=/home/ec2-user/server.log -Dspring.datasource.url=$(cat /home/ec2-user/dbEndpoint.txt) -Djasypt.encryptor.password=$(cat /home/ec2-user/jasyptKey.txt) -Dspring.profiles.active=test -Dserver.port=80 /home/ec2-user/flower-platform.jar > /dev/null 2> /dev/null < /dev/null &
