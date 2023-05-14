b02262fde2d50d3c2b90f6e474db83e7673d0ffa
harryjung <harryjung0330@gmail.com>
2023-05-14 21:55:47 +0900

    added logback and changed appsepc.yaml

--- after_install.sh
+++ after_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 chmod 600 /home/ec2-user/jasyptKey.txt
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -s -m ec2 -c ssm:/test/web-server/config/cloudwatch-config.json
